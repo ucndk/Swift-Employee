290d52930fd87a631aa341ec7cd19779212a147a
Christian Bøtker Høj <christian@skeize.dk>
2014-10-23 16:50:55 +0200

    Initial Commit

---
+++ EmployeeProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EmployeeProject
+//
+//  Created by Christian Bøtker Høj on 23/10/14.
+//  Copyright (c) 2014 UCN. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
